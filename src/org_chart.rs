d73d7072a74d681735da8edd7dabfd2b2504d310
Milo-Shen <Milo-Shen@outlook.com>
2023-11-04 22:08:57 +0800

    feat: add CardNode

--- src/org_chart.rs
+++ src/org_chart.rs
@@ -1,6 +1,25 @@
+use std::cell::RefCell;
+use std::rc::{Rc, Weak};
+
 pub enum CardNodeType {
     NORMAL = 0,
     LITE = 1 << 0,
     BATCH = 1 << 1,
     EXTEND = 1 << 2,
+}
+
+pub struct CardNode<T> {
+    id: i64,
+    content: T,
+    children: RefCell<Vec<Rc<CardNode<T>>>>,
+    parent: RefCell<Weak<CardNode<T>>>,
+    previous: RefCell<Weak<CardNode<T>>>,
+    level: i64,
+    level_previous: RefCell<Weak<CardNode<T>>>,
+    level_first: RefCell<Weak<CardNode<T>>>,
+    width: f32,
+    height: f32,
+    pos_x: f64,
+    pos_y: f64,
+    mode: CardNodeType,
 }
