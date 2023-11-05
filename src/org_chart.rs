4c547688c4f2471e38f8750e10bd5e3fdf58f5fc
Milo-Shen <Milo-Shen@outlook.com>
2023-11-05 11:17:52 +0800

    feat: add org chart rust file

--- src/org_chart.rs
+++ src/org_chart.rs
@@ -1,5 +1,8 @@
 use std::cell::RefCell;
+use std::collections::{HashMap, VecDeque};
 use std::rc::{Rc, Weak};
+
+struct LineNode {}
 
 pub enum CardNodeType {
     NORMAL = 0,
@@ -19,7 +22,35 @@ pub struct CardNode<T> {
     level_first: RefCell<Weak<CardNode<T>>>,
     width: f32,
     height: f32,
-    pos_x: f64,
-    pos_y: f64,
+    pos_x: f32,
+    pos_y: f32,
     mode: CardNodeType,
+}
+
+impl<T> CardNode<T> {
+    pub fn new(id: i64, content: T, w: f32, h: f32, mode: CardNodeType) -> CardNode<T> {
+        CardNode {
+            id,
+            content,
+            children: RefCell::new(vec![]),
+            parent: RefCell::new(Weak::new()),
+            previous: RefCell::new(Weak::new()),
+            level: 0,
+            level_previous: RefCell::new(Weak::new()),
+            level_first: RefCell::new(Weak::new()),
+            width: 0.0,
+            height: 0.0,
+            pos_x: 0.0,
+            pos_y: 0.0,
+            mode,
+        }
+    }
+}
+
+pub struct OrgChart<T> {
+    root: Option<Rc<CardNode<T>>>,
+    previous_card: RefCell<Weak<CardNode<T>>>,
+    card_map: HashMap<i64, Rc<CardNode<T>>>,
+    card_list: RefCell<Vec<Rc<CardNode<T>>>>,
+    card_linked_list: VecDeque<Vec<Rc<CardNode<T>>>>,
 }
