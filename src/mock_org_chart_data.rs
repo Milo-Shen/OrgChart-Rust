ef9960f4808b06471d98adf6465702f402c26921
Milo-Shen <Milo-Shen@outlook.com>
2023-11-06 22:21:38 +0800

    feat: add mock_org_chart_data function

--- src/mock_org_chart_data.rs
+++ src/mock_org_chart_data.rs
@@ -1,3 +1,6 @@
+use std::collections::VecDeque;
+use std::rc::Rc;
+
 pub struct GenerateID {
     id: i64,
 }
@@ -13,4 +16,41 @@ impl GenerateID {
         self.id += 1;
         self.id
     }
+}
+
+pub struct MockChartData {
+    id: i64,
+    children: Vec<i64>,
+}
+
+pub fn build_card() -> MockChartData {
+    let mut generate_id = GenerateID::new();
+    let id = generate_id.get_next_id();
+
+    MockChartData {
+        id,
+        children: vec![],
+    }
+}
+
+pub fn mock_org_chart_data(count: i64, max_child: i64, is_range: bool) -> Vec<MockChartData> {
+    let mut result = vec![];
+    let mut queue = VecDeque::new();
+
+    // generated leaf count
+    let mut remain_count = count - 1;
+
+    // build the root leaf
+    let root = Rc::new(build_card());
+
+    result.push(Rc::clone(&root));
+    queue.push_back(Rc::clone(&root));
+
+    while !queue.is_empty() {
+        let node = queue.pop_front().unwrap();
+        let children = vec![];
+        let child_count = max_child.min(remain_count);
+    }
+
+    vec![]
 }
