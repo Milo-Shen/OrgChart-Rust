f15c22e34da214168a5bfe46d22f11eb334b34d4
Milo-Shen <Milo-Shen@outlook.com>
2023-11-24 22:34:12 +0800

    feat: update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,16 @@ mod utils;
 
 fn main() {
     let start_time = Instant::now();
-    let test = mock_org_chart_data::mock_org_chart_data(300000, 20, false);
+    let mock_data = mock_org_chart_data::mock_org_chart_data(300000, 20, false);
     let duration = start_time.elapsed();
     println!("{:?}", duration);
+    let start_time = Instant::now();
+    let mut chart = org_chart::OrgChart::new(true, 200.0, 100.0, 100.0, 50.0, 10.0, 41.0, 2.0, 2);
+    let duration = start_time.elapsed();
+    println!("{:?}", duration);
+    let start_time = Instant::now();
+    chart.initialization(mock_data);
+    let duration = start_time.elapsed();
+    println!("build org chart time {:?}", duration);
     println!("Hello, world!");
 }
