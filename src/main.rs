320464f7fa5be851c0ad551dcbb573fa4b9fa89e
Milo-Shen <Milo-Shen@outlook.com>
2023-11-24 22:08:29 +0800

    feat: update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ mod utils;
 
 fn main() {
     let start_time = Instant::now();
-    let test = mock_org_chart_data::mock_org_chart_data(3000000, 20, false);
+    let test = mock_org_chart_data::mock_org_chart_data(300000, 20, false);
     let duration = start_time.elapsed();
     println!("{:?}", duration);
     println!("Hello, world!");
